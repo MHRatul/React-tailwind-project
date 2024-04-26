cfd7d91b433bc1a76982b18301bb775df3cccd6f
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-04-26 20:42:10 +0600

    Hero Section Add

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+        <div className='max-w-[800x] mt--[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+          <p> GROWING WITH DATA ANALYTICS </p>
+          <h1>Grow with data with faster...</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
