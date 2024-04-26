a24398d7fb7e9ac34dcc628cf19fad2ec2573c05
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-04-26 12:28:28 +0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-
-import './App.css'
+import React from "react"
+import Navbar from './components/Navbar';
 
 function App() {
-  
 
   return (
-    <>
-    <h1 className="text-5xl font-bold ">
-      Hello world!
-    </h1>
-    </>
-  )
+    <div>
+      <Navbar />
+      <React />
+    </div>
+  );
 }
 
-export default App
+export default App;
