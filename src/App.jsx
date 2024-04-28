4dd4a34c04d26cc3f1b257d33ef1e8c827dd38de
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-04-28 10:13:29 +0600

    card section Respnsive & fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Analytics from './components/Analytics';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Newsletter from './components/Newsletter';
+import Cards from './components/Cards';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Analytics />
       <Newsletter />
+      <Cards />
     </div>
   );
 }
