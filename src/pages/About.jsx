8744e11e69d1fb57f943c41c102036705814a6cb
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-05-10 22:48:03 +0600

    Issue Fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,22 +3,22 @@ import about from '../assets/about.jpeg'
 const About = () => {
   return (
     <div className="text-white max-w-[1240px] mx-auto  px-4 grid lg:grid gap-8">
-      <section class="bg-gray-100">
-        <div class="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
-          <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
-            <div class="max-w-lg">
-              <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">About Us</h2>
-              <p class="mt-4 text-gray-600 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
+      <section className="bg-gray-100">
+        <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
+            <div className="max-w-lg">
+              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">About Us</h2>
+              <p className="mt-4 text-gray-600 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
                 eros at lacus feugiat hendrerit sed ut tortor. Suspendisse et magna quis elit efficitur consequat.
                 Mauris eleifend velit a pretium iaculis. Donec sagittis velit et magna euismod, vel aliquet nulla
                 malesuada. Nunc pharetra massa lectus, a fermentum arcu volutpat vel.</p>
-              <div class="mt-8">
-                <a href="#" class="text-blue-500 hover:text-blue-600 font-medium">Learn more about us
-                  <span class="ml-2">&#8594;</span></a>
+              <div className="mt-8">
+                <a href="#" className="text-blue-500 hover:text-blue-600 font-medium">Learn more about us
+                  <span className="ml-2">&#8594;</span></a>
               </div>
             </div>
-            <div class="mt-12 md:mt-0">
-              <img src={about} alt="About Us Image" class="object-cover rounded-lg shadow-md" />
+            <div className="mt-12 md:mt-0">
+              <img src={about} alt="About Us Image" className="object-cover rounded-lg shadow-md" />
             </div>
           </div>
         </div>
